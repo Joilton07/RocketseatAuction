2da201c86a2a97046bfcedd8bf6f9e2e4a27e5ac
Joilton Alvino <joiltonEstTrab@outlook.com>
2024-06-18 19:49:43 -0300

    Adicionar arquivos de projeto.

---
+++ src/RoceketseatAuction.API/Entities/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RoceketseatAuction.API.Entities;
+
+[Table("Items")]
+public class Item
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Brand { get; set; }
+    public int Condition { get; set; }
+    public int BasePrice { get; set; }
+    public int AuctionId { get; set; }
+}
---
+++ src/RoceketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocektseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction? Execute()
+    {
+        var repository = new RocketseatAuctionDbContext();
+
+        var today = new DateTime(2024, 01, 27);
+
+        return repository
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
+    }
+}
