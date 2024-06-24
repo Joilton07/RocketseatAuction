b3227c1bfe83cc28f1a6b46dcd8f3f33da19eb3f
Joilton Alvino <joiltonEstTrab@outlook.com>
2024-06-24 08:59:02 -0300

    Criação da autenticação do usuário na api

---
+++ src/RoceketseatAuction.API/Controllers/RocketseatAuctionBaseController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoceketseatAuction.API.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class RocketseatAuctionBaseController : ControllerBase
+{
+}
---
+++ src/RoceketseatAuction.API/Filters/AutheticationUserAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RoceketseatAuction.API.Filters;
+
+public class AutheticationUserAttribute : AuthorizeAttribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var token = TokenOnRequest(context.HttpContext);
+    }
+    private string TokenOnRequest(HttpContext context)
+    {
+        var authorization = context.Request.Headers.Authorization.ToString();
+
+        if(string.IsNullOrEmpty(authorization) )
+        {
+            throw new Exception("Token is missing");
+        }
+
+        return authorization["Bearer ".Length..];
+    }
+}
